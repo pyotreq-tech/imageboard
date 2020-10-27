b848495aa5fdc3bc3b8b14fb4fb15d97e34c530b
piotrek_kp <piotrek.kp@gmail.com>
2020-10-27 11:27:48 +0100

    Update

--- index.js
+++ index.js
@@ -1,19 +1,59 @@
 const express = require("express");
 const app = express();
 const db = require("./db");
+const multer = require("multer");
+const uidSafe = require("uid-safe");
+const path = require("path");
 
 app.use(express.static("public"));
+
+const diskStorage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, __dirname + "/uploads");
+    },
+    filename: function (req, file, callback) {
+        uidSafe(24).then(function (uid) {
+            callback(null, uid + path.extname(file.originalname));
+        });
+    },
+});
+
+const uploader = multer({
+    storage: diskStorage,
+    limits: {
+        fileSize: 2097152,
+    },
+});
 
 app.get("/images", (req, res) => {
     db.getImages()
         .then(({ rows }) => {
-            console.log(rows);
+            // console.log(rows);
             res.json(rows);
         })
         .catch((err) => {
             console.log("Error in getImages: ", err);
             res.send("error");
         });
+});
+
+app.post("/images", uploader.single("file"), (req, res) => {
+    console.log("image route reached: ", req.body);
+    console.log("file: ", req.file);
+    // TO DO: insert all the information in the database, then send the information back to Vue:
+    // BUT WE have to wait until afternoon encounter to do that
+    // IT HAVE to work without refreshing
+
+    // we need to send response to Vue, so .then part can run, otherwise it will only run .catch
+    if (req.file) {
+        res.json({
+            success: true,
+        });
+    } else {
+        res.json({
+            success: false,
+        });
+    }
 });
 
 app.listen(8080, () => {
--- public/js/script.js
+++ public/js/script.js
@@ -2,13 +2,48 @@ new Vue({
     el: "#main",
     data: {
         images: [],
+        title: "",
+        description: "",
+        username: "",
+        file: null,
     },
     mounted: function () {
         // important to store this in var so it does not lose its reference
         var me = this;
-        axios.get("/images").then(function (response) {
-            me.images = response.data;
-        });
+        axios
+            .get("/images")
+            .then(function (response) {
+                me.images = response.data;
+            })
+            .catch(function (err) {
+                console.log("err in GET /images: ", err);
+            });
     },
-    methods: {},
+    methods: {
+        handleClick: function (e) {
+            e.preventDefault();
+            // console.log("this!: ", this);
+
+            // we are handling that because of sending a file, otherwise we send just an object
+            var formData = new FormData();
+            formData.append("title", this.title);
+            formData.append("description", this.title);
+            formData.append("username", this.username);
+            formData.append("file", this.file);
+
+            axios
+                .post("/images", formData)
+                .then(function (response) {
+                    console.log("response from /Post: ", response);
+                })
+                .catch(function (err) {
+                    console.log("error in posting images: ", err);
+                });
+        },
+        handleChange: function (e) {
+            // console.log("handleChange is running");
+            // console.log("file: ", e.target.files[0]);
+            this.file = e.target.files[0];
+        },
+    },
 });

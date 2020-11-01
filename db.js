7921a09f3d9ab9dffa3a3496ca33ffa4139ddecb
piotrek_kp <piotrek.kp@gmail.com>
2020-11-01 12:11:11 +0100

    database queries

--- db.js
+++ db.js
@@ -5,19 +5,37 @@ var db = spicedPg(
 );
 
 exports.getImages = () => {
-    return db.query(`SELECT * FROM images ORDER BY id DESC LIMIT 12`);
+    return db.query(`SELECT * FROM images ORDER BY id DESC LIMIT 8`);
 };
+exports.getMoreThanMax = (id) => {
+    return db.query(`SELECT * FROM images WHERE id > $1`, [id]);
+};
+
 exports.getMoreImages = (id) => {
     return db.query(
         `SELECT url, title, id, (
         SELECT id FROM images
         ORDER BY id ASC
         LIMIT 1
-        ) AS "lowestId" FROM images
+        ) AS "lowestId",
+        (
+        SELECT id FROM images
+        ORDER BY id DESC
+        LIMIT 1
+        ) AS "highestId" FROM images
         WHERE id < $1
         ORDER BY id DESC
-        LIMIT 12;
+        LIMIT 8;
 `,
+        [id]
+    );
+};
+exports.getSingleImageModified = (id) => {
+    return db.query(
+        `SELECT id, url, username, title, description, tags,
+        (SELECT id FROM images WHERE id < $1 ORDER BY id DESC LIMIT 1) AS "previousImage",
+        (SELECT id FROM images WHERE id > $1 ORDER BY id ASC LIMIT 1) AS "nextImage"
+        FROM images WHERE id = $1;`,
         [id]
     );
 };
@@ -32,13 +50,21 @@ exports.getSingleImage = (id) => {
     return db.query(`SELECT * FROM images WHERE id = $1`, [id]);
 };
 
-exports.postImage = (url, username, title, description) => {
+// How to make it pass actually good results?
+exports.getImagesByTag = (tag) => {
+    return db.query(`SELECT * FROM images WHERE tags ~ $1`, [tag]);
+};
+exports.deleteImage = (id) => {
+    return db.query(`DELETE FROM images WHERE id = $1`, [id]);
+};
+
+exports.postImage = (url, username, title, description, tags) => {
     return db.query(
-        `INSERT INTO images (url, username, title, description) 
-        VALUES ($1, $2, $3, $4) 
-        RETURNING id, url, title;
+        `INSERT INTO images (url, username, title, description, tags) 
+        VALUES ($1, $2, $3, $4, $5) 
+        RETURNING id, url, title, tags;
 `,
-        [url, username, title, description]
+        [url, username, title, description, tags]
     );
 };
 

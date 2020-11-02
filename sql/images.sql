447b3d7930c5144abdbd6cab7443ac755f6910f5
piotrek_kp <piotrek.kp@gmail.com>
2020-11-02 19:48:06 +0100

    update

--- sql/images.sql
+++ sql/images.sql
@@ -1,19 +1,19 @@
--- DROP TABLE IF EXISTS images CASCADE;
--- CREATE TABLE images(
---     id SERIAL PRIMARY KEY,
---     url VARCHAR NOT NULL,
---     username VARCHAR NOT NULL,
---     title VARCHAR NOT NULL,
---     tags TEXT,
---     description TEXT,
---     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
--- );
+DROP TABLE IF EXISTS images CASCADE;
+CREATE TABLE images(
+    id SERIAL PRIMARY KEY,
+    url VARCHAR NOT NULL,
+    username VARCHAR NOT NULL,
+    title VARCHAR NOT NULL,
+    tags TEXT,
+    description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
--- DROP TABLE IF EXISTS comments CASCADE;
--- CREATE TABLE comments (
---      id SERIAL PRIMARY KEY,
---      comment TEXT NOT NULL, 
---      username TEXT NOT NULL, 
---      imageid INTEGER NOT NULL REFERENCES images(id) ON DELETE CASCADE,
---      time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
--- );
+DROP TABLE IF EXISTS comments CASCADE;
+CREATE TABLE comments (
+     id SERIAL PRIMARY KEY,
+     comment TEXT NOT NULL, 
+     username TEXT NOT NULL, 
+     imageid INTEGER NOT NULL REFERENCES images(id) ON DELETE CASCADE,
+     time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
